f77281ae3e17ad71703adf9d0553401a3120b95f
Alven08 <alvarosoto0823@gmail.com>
2024-11-30 18:26:39 -0500

    small fix in product popup

--- AmazPocket/static/AmazPocket/product.js
+++ AmazPocket/static/AmazPocket/product.js
@@ -16,15 +16,6 @@ function onProductClick(product_id) {
 
             document.getElementById('product-description').innerHTML = data.description;
             document.getElementById('id_price').innerHTML = `<strong>Price:</strong> $${data.price}`;
-
-            // setUpProductForm(data);
-            // document.getElementById('selected-product-img').src = data.img_url;
-            // document.getElementById('exampleModal').innerHTML = data.name;
-            //
-            // document.getElementById('id_category').value = data.category.id;
-            // document.getElementById('id_img_url').value = data.img_url;
-            // document.getElementById('id_price').value = data.price;
-            // document.getElementById('id_in_stock_quantity').value = data.in_stock_quantity;
-            // document.getElementById('id_is_active').checked = data.is_active;
+            document.getElementById('exampleModalLabel').innerHTML = data.name;
         });
 }
